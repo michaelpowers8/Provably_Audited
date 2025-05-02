24e2f500c10e928226298e2744c953583fb8bc80
Michael Powers <126989418+michaelpowers8@users.noreply.github.com>
2025-05-01 21:51:52 -0500

    Update Dice_Simulation.py

--- Stake_Dice/Dice_Simulation.py
+++ Stake_Dice/Dice_Simulation.py
@@ -3,6 +3,7 @@ import hashlib
 import random
 import string
 from math import floor
+from numpy import mean,median,quantile
 import json
 from pandas import DataFrame
 from Mulitpliers import dice_multipliers
@@ -83,14 +84,15 @@ if __name__ == "__main__":
     with open("Configuration.json","rb") as file:
         configuration:dict[str,str|int] = json.load(file)
 
-    server:str = configuration["ServerSeed"]
+    server:str = generate_server_seed()#configuration["ServerSeed"]
     server_hashed:str = sha256_encrypt(server)
-    client:str = configuration["ClientSeed"]
+    client:str = generate_client_seed()#configuration["ClientSeed"]
     nonces:list[int] = list(range(configuration["MinimumNonce"],configuration["MaximumNonce"]+1))
     over_under:str = configuration["OverUnder"]
     threshold:float = round(configuration["Threshold"],2)
     win_chance:float = round(configuration["WinChance"],2)
     bet_size:float = configuration["BetSize"]
+    largest_bet_size:float = bet_size
 
     if(confirm_threshold_with_win_chance(over_under=over_under, threshold=threshold, win_chance=win_chance)):
         pass
@@ -105,18 +107,34 @@ if __name__ == "__main__":
 
     biggest_winning_streak:tuple[int,int] = (0,0)
     biggest_losing_streak:tuple[int,int] = (0,0)
+
     current_winning_streak:int = 0
     current_losing_streak:int = 0
+    losing_streak_sizes:list[int] = []
+
     total_number_of_wins:int = 0
     total_number_of_losses:int = 0
     total_games_played:int = 0
+
+    balance:float = 10000
+    starting_balance:float = balance
+    biggest_balance:float = balance
+
+    money_spent_over_current_losing_streak:float = 0
+    most_money_spent_over_current_losing_streak:float = 0
+
     money_won:float = 0
+    money_bet:float = 0
     nonces_with_perfect_rolls:list[int] = []
 
     for nonce in nonces:
         total_games_played += 1
         current_result = [server,client,nonce]
         seed_result = seeds_to_results(server_seed=server,client_seed=client,nonce=nonce)
+        balance -= bet_size
+        money_bet += bet_size
+        if(balance < 0):
+            break
         if(
             (
                 (seed_result <= threshold)and
@@ -132,15 +150,24 @@ if __name__ == "__main__":
                 biggest_winning_streak = (nonce-current_winning_streak,current_winning_streak)
             current_winning_streak = 0
             current_losing_streak += 1
-            total_number_of_losses +=1
+            total_number_of_losses += 1
+            money_spent_over_current_losing_streak += bet_size
             current_result.extend([seed_result,"NO"])
         else:
             if(current_losing_streak > biggest_losing_streak[1]):
                 biggest_losing_streak = (nonce-current_losing_streak,current_losing_streak)
+            if(money_spent_over_current_losing_streak > most_money_spent_over_current_losing_streak):
+                most_money_spent_over_current_losing_streak = money_spent_over_current_losing_streak
+            if(current_losing_streak>0):
+                losing_streak_sizes.append(current_losing_streak)
             current_losing_streak = 0
+            money_spent_over_current_losing_streak = 0
             current_winning_streak += 1
             total_number_of_wins += 1
             money_won += (bet_size*dice_multipliers[over_under][float(f"{threshold:.2f}")])
+            balance += bet_size*dice_multipliers[over_under][float(f"{threshold:.2f}")]
+            if(balance > biggest_balance):
+                biggest_balance = balance
             current_result.extend([seed_result,"YES"])
         if(
             (
@@ -162,16 +189,28 @@ Server Seed: {server}
 Server Seed (Hashed): {server_hashed}
 Client Seed: {client}
 Nonces: {nonces[0]:,.0f} - {nonces[-1]:,.0f}
+Initial Balance: ${starting_balance:,.2f}
+Ending Balance: ${balance if balance>0 else 0:,.2f}
+Largest Balance Ever: ${biggest_balance:,.2f}
 Bet Size: ${bet_size:,.2f}
+Largest Bet Made: ${largest_bet_size:,.2f}
+Most Money Spent Over Losing Streak: ${most_money_spent_over_current_losing_streak:,.2f}
 Winning Multiplier: {dice_multipliers[over_under][float(f"{threshold:.2f}")]:,.4f}
 Net Profit per Win: ${bet_size*dice_multipliers[over_under][float(f"{threshold:.2f}")] - bet_size:,.2f}
 Number of games simulated: {total_games_played:,.0f}
 Number of wins: {total_number_of_wins:,.0f}
 Number of Losses: {total_number_of_losses:,.0f}
+Mean Losing Streak: {mean(losing_streak_sizes):,.3f}
+Median Losing Streak: {median(losing_streak_sizes):,.1f}
+Statistical Summary of Losing Streaks:
+\tMin\t\t|\t\t25%\t\t|\t\t50%\t\t|\t\t75%\t\t|\t\t95%\t\t|\t\t99%\t\t|\t\tMax
+\t{min(losing_streak_sizes):,.0f}\t\t|\t\t{quantile(losing_streak_sizes,0.25):,.0f}\t\t|\t\t{median(losing_streak_sizes):,.0f}\t\t|\t\t{quantile(losing_streak_sizes,0.75):,.0f}\t\t|\t\t{quantile(losing_streak_sizes,0.95):,.0f}\t\t|\t\t{quantile(losing_streak_sizes,0.99):,.0f}\t\t|\t\t{max(losing_streak_sizes):,.0f}
 Biggest Winning Streak: {biggest_winning_streak[1]:,.0f}
 Starting Nonce of Biggest Winning Streak: {biggest_winning_streak[0]:,.0f}
 Biggest Losing Streak: {biggest_losing_streak[1]:,.0f}
 Starting Nonce of Biggest Losing Streak: {biggest_losing_streak[0]:,.0f}
 With ${bet_size:,.2f} bets, gross winnings: ${money_won:,.2f}
-With ${bet_size:,.2f} bets, net result: ${abs(money_won-(bet_size*total_games_played)):,.2f} {"won" if money_won-(bet_size*total_games_played)>0 else "lost"}.
-Number of perfect {100 if over_under=="Over" else 0} rolls: {len(nonces_with_perfect_rolls):,.0f}""")
+With ${bet_size:,.2f} bets, net result: ${abs(money_won-money_bet):,.2f} {"won" if money_won-money_bet>0 else "lost"}.
+Number of perfect {100 if over_under=="Over" else 0} rolls: {len(nonces_with_perfect_rolls):,.0f}
+
+""")
